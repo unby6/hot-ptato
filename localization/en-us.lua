17a5e8f47dd97760c23899e2c23698d16f23ea87
Ruby <darkdragongaming379@gmail.com>
2025-07-25 15:55:30 +0100

    credits system

---
+++ localization/en-us.lua
@@ -0,0 +1,10 @@
+return {
+    misc = {
+        v_dictionary = {
+            hotpot_art = { "Art: #1#" },
+            hotpot_code = { "Code: #1#" },
+            hotpot_idea = { "Idea: #1#" },
+            hotpot_team = { "Team: #1#" }
+        }
+    }
+}
--- main.lua
+++ main.lua
@@ -45,3 +45,115 @@ local path = SMODS.current_mod.path
 
 load_files(path, true)
 --#endregion
+
+-- CREDITS SYSTEM
+--#region Card Credits System
+local smcmb = SMODS.create_mod_badges
+function SMODS.create_mod_badges(obj, badges)
+	smcmb(obj, badges)
+	if not SMODS.config.no_mod_badges and obj and obj.hotpot_credits then
+		local function calc_scale_fac(text)
+			local size = 0.9
+			local font = G.LANG.font
+			local max_text_width = 2 - 2 * 0.05 - 4 * 0.03 * size - 2 * 0.03
+			local calced_text_width = 0
+			-- Math reproduced from DynaText:update_text
+			for _, c in utf8.chars(text) do
+				local tx = font.FONT:getWidth(c) * (0.33 * size) * G.TILESCALE * font.FONTSCALE
+					+ 2.7 * 1 * G.TILESCALE * font.FONTSCALE
+				calced_text_width = calced_text_width + tx / (G.TILESIZE * G.TILESCALE)
+			end
+			local scale_fac = calced_text_width > max_text_width and max_text_width / calced_text_width or 1
+			return scale_fac
+		end
+		if obj.hotpot_credits.art or obj.hotpot_credits.code or obj.hotpot_credits.idea or obj.hotpot_credits.custom then
+			local scale_fac = {}
+			local min_scale_fac = 1
+			local strings = { HotPotato.display_name }
+			for _, v in ipairs({ "idea", "art", "code", "team" }) do
+				if obj.hotpot_credits[v] then
+					if type(obj.hotpot_credits[v]) == "string" then obj.hotpot_credits[v] = {obj.hotpot_credits[v]} end
+					for i = 1, #obj.hotpot_credits[v] do
+						strings[#strings + 1] =
+							localize({ type = "variable", key = "hotpot_" .. v, vars = { obj.hotpot_credits[v][i] } })[1]
+					end
+				end
+			end
+            if obj.hotpot_credits.custom then
+                strings[#strings + 1] = localize({ type="variable", key = obj.hotpot_credits.custom.key, vars = { obj.hotpot_credits.custom.text } })
+            end
+			for i = 1, #strings do
+				scale_fac[i] = calc_scale_fac(strings[i])
+				min_scale_fac = math.min(min_scale_fac, scale_fac[i])
+			end
+			local ct = {}
+			for i = 1, #strings do
+				ct[i] = {
+					string = strings[i],
+				}
+			end
+			local hotpot_badge = {
+				n = G.UIT.R,
+				config = { align = "cm" },
+				nodes = {
+					{
+						n = G.UIT.R,
+						config = {
+							align = "cm",
+							colour = HotPotato.badge_colour,
+							r = 0.1,
+							minw = 2 / min_scale_fac,
+							minh = 0.36,
+							emboss = 0.05,
+							padding = 0.03 * 0.9,
+						},
+						nodes = {
+							{ n = G.UIT.B, config = { h = 0.1, w = 0.03 } },
+							{
+								n = G.UIT.O,
+								config = {
+									object = DynaText({
+										string = ct or "ERROR",
+										colours = { obj.hotpot_credits and obj.hotpot_credits.text_colour or G.C.WHITE },
+										silent = true,
+										float = true,
+										shadow = true,
+										offset_y = -0.03,
+										spacing = 1,
+										scale = 0.33 * 0.9,
+									}),
+								},
+							},
+							{ n = G.UIT.B, config = { h = 0.1, w = 0.03 } },
+						},
+					},
+				},
+			}
+			local function eq_col(x, y)
+				for i = 1, 4 do
+					if x[i] ~= y[i] then
+						return false
+					end
+				end
+				return true
+			end
+			for i = 1, #badges do	
+				if badges[i].nodes[1].nodes[2].config.object.string == HotPotato.display_name then --this was meant to be a hex code but it just doesnt work for like no reason so its hardcoded
+					badges[i].nodes[1].nodes[2].config.object:remove()
+					badges[i] = hotpot_badge
+					break
+				end
+			end
+		end
+	end
+end
+--#endregion
+
+SMODS.Joker:take_ownership("j_joker", {
+	hotpot_credits = {
+		art = {"Test"},
+		idea = {"Test"},
+		code = {"Test"},
+		team = {"Test"}
+	}
+})
