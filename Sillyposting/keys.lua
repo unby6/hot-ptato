2d07c30531742e036e6a255ede9a75618a0178cb
Eris (borb) <remcosveen@gmail.com>
2025-08-09 20:44:04 +0200

    did colour and collection row stuff for keys these things are mostly temporary for now

--- Sillyposting/keys.lua
+++ Sillyposting/keys.lua
@@ -1,5 +1,8 @@
 SMODS.ConsumableType {
     key = "key",
-    primary_colour = HEX("d4af37"),
-    secondary_colour = HEX("c0c0c0")
+    primary_colour = G.C.GOLD,
+    secondary_colour = HEX("c0c0c0"),
+    collection_rows = { 4, 4 } --temporary!! feel free to change later depending on the number of keys
 }
+--just use {C:gold} for text using the primary colour
+G.ARGS.LOC_COLOURS["silver"] = G.C.SECONDARY_SET.key
