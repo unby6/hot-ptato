6a241db56ed3a1417903fb3db58463f06e4749f3
Eris (borb) <remcosveen@gmail.com>
2025-08-09 23:41:48 +0200

    stop being fucking stupid dont try to add a value to G.GAME outside of the game

--- Sillyposting/lib.lua
+++ Sillyposting/lib.lua
@@ -1,16 +1,15 @@
 --for modifying max_highlights of consumables. *should* work with any modded consumeables that use the usual max_highlighted system
 --#region Max Highlight stuff
 --please fucking work PLEASE
-G.GAME.max_highlighted_mod = 0
 
 create_card_ref = create_card
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     local card = create_card_ref(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     if card.ability.consumeable and card.ability.consumeable.max_highlighted then
-        card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted + G.GAME.max_highlighted_mod
+        card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted + (G.GAME.max_highlighted_mod or 0)
         if card.ability.consumeable.max_highlighted < 1 then card.ability.consumeable.max_highlighted = 1 end
     elseif card.ability and card.ability.max_highlighted then
-        card.ability.max_highlighted = card.ability.max_highlighted + G.GAME.max_highlighted_mod
+        card.ability.max_highlighted = card.ability.max_highlighted + (G.GAME.max_highlighted_mod or 0)
         if card.ability.max_highlighted < 1 then card.ability.max_highlighted = 1 end
     end
     return card
