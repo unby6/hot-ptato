9ce1f0d10e59bf6ffc8f91f188738f4866111fcd
Eris (borb) <remcosveen@gmail.com>
2025-08-10 17:58:28 +0200

    check if card.ability.extra is a table

--- Sillyposting/lib.lua
+++ Sillyposting/lib.lua
@@ -6,13 +6,18 @@ create_card_ref = create_card
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     local card = create_card_ref(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     if card.ability then
-        if card.ability.consumeable and card.ability.consumeable.max_highlighted then
-            card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted +
-            (G.GAME.max_highlighted_mod or 0)
-            if card.ability.consumeable.max_highlighted < 1 then card.ability.consumeable.max_highlighted = 1 end
-        elseif card.ability.extra and card.ability.extra.max_highlighted then
-            card.ability.extra.max_highlighted = card.ability.extra.max_highlighted + (G.GAME.max_highlighted_mod or 0)
-            if card.ability.extra.max_highlighted < 1 then card.ability.extra.max_highlighted = 1 end
+        if card.ability.consumeable and type(card.ability.consumeable) == "table" then
+            if card.ability.consumeable.max_highlighted then
+                card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted +
+                    (G.GAME.max_highlighted_mod or 0)
+                if card.ability.consumeable.max_highlighted < 1 then card.ability.consumeable.max_highlighted = 1 end
+            end
+        elseif card.ability.extra and type(card.ability.extra) == "table" then
+            if card.ability.extra.max_highlighted then
+                card.ability.extra.max_highlighted = card.ability.extra.max_highlighted +
+                    (G.GAME.max_highlighted_mod or 0)
+                if card.ability.extra.max_highlighted < 1 then card.ability.extra.max_highlighted = 1 end
+            end
         end
     end
     return card
