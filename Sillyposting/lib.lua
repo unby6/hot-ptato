35c0b1dac500378bf1f6ad54295980a489143a2b
Eris (borb) <remcosveen@gmail.com>
2025-08-10 17:53:16 +0200

    properly check for card.ability fields

--- Sillyposting/lib.lua
+++ Sillyposting/lib.lua
@@ -5,12 +5,15 @@
 create_card_ref = create_card
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     local card = create_card_ref(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    if card.ability.consumeable and card.ability.consumeable.max_highlighted then
-        card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted + (G.GAME.max_highlighted_mod or 0)
-        if card.ability.consumeable.max_highlighted < 1 then card.ability.consumeable.max_highlighted = 1 end
-    elseif card.ability and card.ability.extra.max_highlighted then
-        card.ability.extra.max_highlighted = card.ability.extra.max_highlighted + (G.GAME.max_highlighted_mod or 0)
-        if card.ability.extra.max_highlighted < 1 then card.ability.extra.max_highlighted = 1 end
+    if card.ability then
+        if card.ability.consumeable and card.ability.consumeable.max_highlighted then
+            card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted +
+            (G.GAME.max_highlighted_mod or 0)
+            if card.ability.consumeable.max_highlighted < 1 then card.ability.consumeable.max_highlighted = 1 end
+        elseif card.ability.extra and card.ability.extra.max_highlighted then
+            card.ability.extra.max_highlighted = card.ability.extra.max_highlighted + (G.GAME.max_highlighted_mod or 0)
+            if card.ability.extra.max_highlighted < 1 then card.ability.extra.max_highlighted = 1 end
+        end
     end
     return card
 end
