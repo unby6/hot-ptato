4dddde7d8d28aa4c5bdb9b9f851382b7172aafc6
unusedparadox <unusedparadox@gmail.com>
2025-08-10 11:57:44 -0700

    Fixed more misplaced localization brackets. Seriously how hard is it to write a localization file

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -385,6 +385,7 @@ return {
                     "Play all to earn {C:money}$#2#"
                 }
             },
+        },
         Voucher = {
             v_hpot_currency_exchange = {
                 ['name'] = 'Currency Exchange',
@@ -460,7 +461,6 @@ return {
                     "{C:hpot_czech}Mega Cheque Pack",
                 },
             },
-        },
         },
     },
     misc = {
