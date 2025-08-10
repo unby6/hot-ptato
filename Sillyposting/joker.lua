40b647bc33768ba4c0e757e5556521f6ab0fd3dc
Eris (borb) <remcosveen@gmail.com>
2025-08-10 17:21:21 +0200

    start work on wizard tower increases consumable max highlights

---
+++ Sillyposting/joker.lua
@@ -0,0 +1,20 @@
+SMODS.Atlas({ key = "SillypostingJokers", path = "Sillyposting/Jokers.png", px = 71, py = 95, atlas_table = "ASSET_ATLAS" }):register()
+
+SMODS.Joker {
+    key = "wizard_tower",
+    blueprint_compat = false,
+    rarity = 3,
+    cost = 10,
+    atlas = "SillypostingJokers",
+    pos = { x = 0, y = 0 },
+    config = { extra = { bonus_highlight = 1 } },
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.bonus_highlight } }
+    end,
+    add_to_deck = function (self, card, from_debuff)
+        change_max_highlight(card.ability.extra.bonus_highlight)
+    end,
+    remove_from_deck = function (self, card, from_debuff)
+        change_max_highlight(-card.ability.extra.bonus_highlight)
+    end
+}
