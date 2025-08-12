d719ccf97dac714b22cb0c38e7e9e12bdfd412a6
Eris (borb) <remcosveen@gmail.com>
2025-08-12 21:05:33 +0200

    lib.lua fixes

--- Sillyposting/lib.lua
+++ Sillyposting/lib.lua
@@ -5,7 +5,7 @@
 create_card_ref = create_card
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     local card = create_card_ref(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    if card.ability then
+    if card.ability and type(card.ability) == "table" then
         if card.ability.consumeable and type(card.ability.consumeable) == "table" then
             if card.ability.consumeable.max_highlighted then
                 card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted +
@@ -28,12 +28,12 @@ end
 function change_max_highlight(amount) --modifies the max_highlighted_mod variable and updates all existing consumables automatically. mostly for simplicity
     G.GAME.max_highlighted_mod = (G.GAME.max_highlighted_mod or 0) + amount
     for _, card in pairs(G.I.CARD) do
-        if card.ability then
+        if card.ability and type(card.ability) == "table" then
             if card.ability.consumeable and card.ability.consumeable.max_highlighted then
                 card.ability.consumeable.max_highlighted = card.ability.consumeable.max_highlighted + amount
                 if card.ability.consumeable.max_highlighted < 1 then card.ability.consumeable.max_highlighted = 1 end
             elseif card.ability.extra and type(card.ability.extra) == "table" then
-                if card.ability.max_highlighted then
+                if card.ability.extra.max_highlighted then
                     card.ability.extra.max_highlighted = card.ability.extra.max_highlighted + amount
                     if card.ability.extra.max_highlighted < 1 then card.ability.extra.max_highlighted = 1 end
                 end
