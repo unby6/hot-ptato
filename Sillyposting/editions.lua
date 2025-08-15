bc1c9ac5b5ff71b5513bb247fa5308dfa0192e87
Eris (borb) <remcosveen@gmail.com>
2025-08-15 21:58:30 +0200

    finish up psychedelic edition

--- Sillyposting/editions.lua
+++ Sillyposting/editions.lua
@@ -2,3 +2,23 @@ SMODS.Shader {
     key = "psychedelic",
     path = "psychedelic.fs"
 }
+
+SMODS.Edition { --doesnt have unique sound yet!
+    key = "psychedelic",
+    shader = "psychedelic",
+    config = { odds = 3 },
+    weight = 3,
+    extra_cost = 5,
+    loc_vars = function (self, info_queue, card)
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.edition.odds, "hpot_psych")
+        return { vars = { numerator, denominator } }
+    end,
+    calculate = function (self, card, context)
+        if context.before and context.main_eval and SMODS.pseudorandom_probability(card, "hpot_psych", 1, card.edition.odds ) then
+            return {
+                level_up = true,
+                message = "k_upgrade_ex"
+            }
+        end
+    end
+}
