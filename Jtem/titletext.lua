994f7a8993be9212e591efb882ac7e8202a5181f
squidguset <tdhc100@gmail.com>
2025-08-16 12:34:08 -0500

    hey folks

--- Jtem/titletext.lua
+++ Jtem/titletext.lua
@@ -6,10 +6,19 @@ HPJTTT = {
         "Also try Nubbys Number Factory!",
         "HotPot Edition",
         "Joker Poker",
-        "The Original                                      Rougelike"
+        "The Original                                      Rougelike",
+        "18+",
+        "Plant Vs Faces",
+        "how LOVELY",
+        "guess you're really feeling the BALATRO",
+        "Hey folks! In today's video we're back with another unseeded high score Balatro run. Here, after running a lot of Baron/Mime builds recently, I decided to work towards an Idol after receiving a lot of Queens and several Immolates in the early game. The result was one of the best Flush Five builds I've ever put together. Enjoy the video! ",
+        "Ship It",
+        "Go Next"
     }
 }
 
 
 
 HPJTTT.chosen = (math.floor(os.time())%#HPJTTT.text)+1
+
+HPJTTT.balala = (math.floor(os.time())%100) == 0
