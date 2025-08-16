d1c45eadc5d02770d315efb5bfd0c194ad486abe
Eris (borb) <remcosveen@gmail.com>
2025-08-16 14:56:26 +0200

    last minute blind addition i hope this works

--- Sillyposting/blinds.lua
+++ Sillyposting/blinds.lua
@@ -1,1 +1,16 @@
 SMODS.Atlas({ key = "SillypostingBlinds", path = "Sillyposting/Blinds.png", px = 34, py = 34, asset_table = "ANIMATION_ATLAS", frames = 21}):register()
+
+SMODS.Blind {
+    key = "quartz",
+    atlas = "SillypostingBlinds",
+    pos = { x= 0, y = 0 },
+    dollars = 5,
+    mult = 2,
+    boss = { min = 3 },
+    boss_colour = HEX("4DD8B5"),
+    calculate = function(self, blind, context)
+        if not blind.disabled and context.press_play then
+            ease_plincoins(-1)
+        end
+    end
+}
