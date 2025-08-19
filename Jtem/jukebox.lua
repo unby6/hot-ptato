d89bb515a21a268c542d7b498ae28720d67c780a
hayaunderscore <stophayapls123@proton.me>
2025-08-19 17:45:48 +0800

    feat: jukebox marquee smooth scrolling
    also docs

--- Jtem/jukebox.lua
+++ Jtem/jukebox.lua
@@ -3,22 +3,34 @@
 require 'bit'
 base64 = SMODS.load_file("JTem/base64.lua")()
 
+---@class Jtem.MusicTag
+---@field title string? Title of the music provided. Can be empty.
+---@field artist string? Artist of the music provided. Can be empty.
+---@field key string
+
 JTJukebox = {}
+---@type Jtem.MusicTag[]
 JTJukebox.MusicTags = {}
 
--- integers in vorbis comments are laid out as little endian
+-- Turns a 4 character long byte array in little endian into a unsigned 32 bit integer
+---@param str string Byte array represented as a string
+---@return number
 function get_32_bit_integer_le(str)
 	local l, m, n, o = string.byte(str, 1, 4)
 	return l + (bit.lshift(m, 8)) + (bit.lshift(n, 16)) + (bit.lshift(o, 24))
 end
 
--- integers in FLAC picture blocks are big endian
+-- Turns a 4 character long byte array in big endian into a unsigned 32 bit integer
+---@param str string Byte array represented as a string
+---@return number
 function get_32_bit_integer_be(str)
 	local l, m, n, o = string.byte(str, 1, 4)
 	return (bit.lshift(l, 24)) + (bit.lshift(m, 16)) + (bit.lshift(n, 8)) + o
 end
 
--- decodes an image from a vorbis comment
+-- Decodes a base64 image from a vorbis comment
+---@param str string Base64 string to decode
+---@param name string Atlas name identifier
 function decode_vorbis_comment_img(str, name)
 	local decoded = base64.decode(str)
 	local idx = 1
@@ -64,18 +76,22 @@ function decode_vorbis_comment_img(str, name)
 	}
 end
 
--- Create
+-- Creates a MusicTag from a desired path. Also creates its respective cover image if applicable.
+---@param path string Path to the music file.
+---@param music_name string Identifier for the MusicTag.
+---@return Jtem.MusicTag
 function JTJukebox.read_music_tags(path, music_name)
 	local data = NFS.read('data', path)
 	if not data then return {} end
 	local str = data:getString() -- byte arrays aren't a thing in lua :P
 	-- find first 'vorbis' string. determines if its actually a vorbis file
 	local idx = string.find(str, 'vorbis')
-	local info = {}
+	---@type Jtem.MusicTag
+	local info = { key = music_name }
 	if idx > 0 then
 		-- find second vorbis string
 		local v = string.find(str, 'vorbis', idx + 6)
-		if not v then return {} end
+		if not v then return info end
 		idx = v + 6
 		-- Skip over vendor_string
 		local vendor_length = get_32_bit_integer_le(string.sub(str, idx, idx + 4))
@@ -243,11 +259,11 @@ JTJukebox.manual_parse = function(text, args)
 	end
 end
 
-function G.FUNCS.hpot_jukebox_go_back(e)
-	JTJukebox.Current = JTJukebox.Current - 1
-	if JTJukebox.Current <= 0 then JTJukebox.Current = #JTJukebox.MusicTags end
-	local current = JTJukebox.MusicTags[JTJukebox.Current]
-	JTJukebox.CurrentDesc = JTJukebox.manual_parse(
+-- Creates a description from a MusicTag.
+---@param current Jtem.MusicTag The MusicTag to use.
+---@return balatro.Loc.ParsedEntry|{ [number]: balatro.Loc.Parsed[] |nil}
+function JTJukebox.CreateDescription(current)
+	return JTJukebox.manual_parse(
 		{
 			"{s:1.1}" .. (current.title or current.key),
 			"{C:dark_edition}" .. (current.artist or "")
@@ -256,51 +272,57 @@ function G.FUNCS.hpot_jukebox_go_back(e)
 	)
 end
 
+function G.FUNCS.hpot_jukebox_go_back(e)
+	JTJukebox.Current = JTJukebox.Current - 1
+	if JTJukebox.Current <= 0 then JTJukebox.Current = #JTJukebox.MusicTags end
+	JTJukebox.CurrentDesc = JTJukebox.CreateDescription(JTJukebox.MusicTags[JTJukebox.Current])
+end
+
 function G.FUNCS.hpot_jukebox_go_next(e)
 	JTJukebox.Current = JTJukebox.Current + 1
 	if JTJukebox.Current > #JTJukebox.MusicTags then JTJukebox.Current = 1 end
-	local current = JTJukebox.MusicTags[JTJukebox.Current]
-	JTJukebox.CurrentDesc = JTJukebox.manual_parse(
-		{
-			"{s:1.1}" .. (current.title or current.key),
-			"{C:dark_edition}" .. (current.artist or "")
-		},
-		{ loc_key = current.key }
-	)
+	JTJukebox.CurrentDesc = JTJukebox.CreateDescription(JTJukebox.MusicTags[JTJukebox.Current])
 end
 
 function G.FUNCS.hpot_update_current(e)
 	if JTJukebox.LastCurrentDesc ~= JTJukebox.CurrentDesc then
 		JTJukebox.LastCurrentDesc = JTJukebox.CurrentDesc
 		local current = JTJukebox.MusicTags[JTJukebox.Current]
+
+		-- get song info element
 		---@type balatro.UIElement
 		local song_info = e.UIBox:get_UIE_by_ID('hpot_jukebox_songinfo')
 		-- clear all children
 		song_info.children = {}
+
 		local final_line = {}
 		for _, line in ipairs(JTJukebox.CurrentDesc) do
 			final_line[#final_line + 1] = {
 				n = G.UIT.R,
-				config = { align = "cm", padding = 0.03 },
+				config = { align = "cm", padding = 0.06 },
 				nodes = SMODS
 					.localize_box(line, {})
 			}
 		end
+
+		-- create a new uibox for the song info description
 		song_info.children[#song_info.children + 1] = UIBox {
 			definition = {
 				n = G.UIT.C,
-				config = { align = "cm", minh = 0.8, minw = 3, colour = G.C.WHITE, r = 0.2, emboss = 0.05 },
+				config = { align = "cm", minh = 1, minw = 4, colour = G.C.WHITE, r = 0.2, emboss = 0.05 },
 				nodes = final_line,
 			}, config = { parent = song_info, align = "cm", major = song_info }
 		}
 		e.UIBox:recalculate()
-		
+
 		-- update cover art
 		local cover = e.UIBox:get_UIE_by_ID('hpot_jukebox_cover_art')
 		cover.config.object.atlas = G.ASSET_ATLAS[current.key] or G.ASSET_ATLAS["hpot_jukebox_default"]
 	end
 end
 
+---@param config table
+---@return table
 function create_center_aligned_text(config)
 	return {
 		n = G.UIT.R,
@@ -317,13 +339,13 @@ end
 function G.FUNCS.hpot_jukebox_play(e)
 	local current = JTJukebox.MusicTags[JTJukebox.Current]
 	if not JTJukebox.ActuallyPlaying then
-		JTJukebox.CurrentlyPlaying = (current.title and (current.title.." - "..current.artist) or current.key)
+		JTJukebox.CurrentlyPlaying = (current.title and (current.title .. " - " .. current.artist) or current.key)
 		JTJukebox.ActuallyPlaying = current.key
-		e.children[1].children[1].config.text = "Unrequest Song"
+		e.children[1].children[1].config.text = localize('hotpot_jukebox_unrequest')
 	else
-		JTJukebox.CurrentlyPlaying = "-#- JUKEBOX -#-"
+		JTJukebox.CurrentlyPlaying = localize('hotpot_jukebox_default_music_title')
 		JTJukebox.ActuallyPlaying = nil
-		e.children[1].children[1].config.text = "Request Song"
+		e.children[1].children[1].config.text = localize('hotpot_jukebox_request')
 	end
 	local jukebox_text = e.UIBox:get_UIE_by_ID('hpot_jukebox_text')
 	jukebox_text.config.object:update_text(true)
@@ -332,15 +354,35 @@ end
 
 -- Jukebox tab lmao
 function JTJukebox.MusicTab()
+	JTJukebox.CurrentDesc = JTJukebox.CreateDescription(JTJukebox.MusicTags[JTJukebox.Current])
 	local current = JTJukebox.MusicTags[JTJukebox.Current]
-	JTJukebox.CurrentDesc = JTJukebox.manual_parse(
-		{
-			"{s:1.1}" .. (current.title or current.key),
-			"{C:dark_edition}" .. (current.artist or "")
+	JTJukebox.CurrentlyPlaying = JTJukebox.CurrentlyPlaying or localize('hotpot_jukebox_default_music_title')
+	local titleDynaText = DynaText {
+		string = {
+			{
+				ref_table = JTJukebox,
+				ref_value = "CurrentlyPlaying"
+			}
 		},
-		{ loc_key = current.key }
-	)
-	JTJukebox.CurrentlyPlaying = JTJukebox.CurrentlyPlaying or "-#- JUKEBOX -#-"
+		colours = { G.C.DARK_EDITION },
+		bump = false,
+		silent = true,
+		pop_in_rate = 99999,
+		spacing = 2,
+		maxw = 7.25,
+		does_scroll = true,
+		non_recalc = true,
+		shadow = true,
+	}
+	titleDynaText.config.stencil = function()
+		love.graphics.rectangle(
+			"fill",
+			titleDynaText.T.x * G.TILESIZE * G.TILESCALE,
+			titleDynaText.T.y * G.TILESIZE * G.TILESCALE,
+			7.25 * G.TILESIZE * G.TILESCALE,
+			10 * G.TILESIZE * G.TILESCALE
+		)
+	end
 	return {
 		n = G.UIT.ROOT,
 		config = { colour = G.C.CLEAR },
@@ -360,7 +402,7 @@ function JTJukebox.MusicTab()
 						n = G.UIT.C,
 						config = {
 							r = 0.1,
-							minw = 7.5,
+							minw = 7.75,
 							minh = 5.5,
 							align = "tm",
 							padding = 0.2,
@@ -372,31 +414,18 @@ function JTJukebox.MusicTab()
 								n = G.UIT.R,
 								config = {
 									r = 0.1,
-									minw = 7.5,
+									minw = 7.75,
 									minh = 1,
 									align = "cm",
 									padding = 0.2,
-									colour = darken(G.C.BLACK, 0.2)
+									colour = darken(G.C.BLACK, 0.2),
+									emboss = 0.05,
 								},
 								nodes = {
 									{
 										n = G.UIT.O,
 										config = {
-											object = DynaText {
-												string = {
-													{
-														ref_table = JTJukebox,
-														ref_value = "CurrentlyPlaying"
-													}
-												},
-												colours = { G.C.DARK_EDITION },
-												bump = false,
-												silent = true,
-												pop_in_rate = 99999,
-												spacing = 2,
-												maxw = 7.25,
-												marquee = true,
-											},
+											object = titleDynaText,
 											id = 'hpot_jukebox_text'
 										}
 									},
@@ -453,7 +482,7 @@ function JTJukebox.MusicTab()
 										n = G.UIT.C,
 										config = { align = "cm", minh = 0.8, minw = 0.8, colour = G.C.RED, r = 0.2, emboss = 0.05, button = 'hpot_jukebox_go_back' },
 										nodes = {
-											create_center_aligned_text{ align = "cm", text = "<", colour = G.C.UI.TEXT_LIGHT, maxw = 0.7, scale = 0.75, shadow = true }
+											create_center_aligned_text { align = "cm", text = "<", colour = G.C.UI.TEXT_LIGHT, maxw = 0.7, scale = 0.75, shadow = true }
 										}
 									},
 									-- play butan
@@ -461,7 +490,7 @@ function JTJukebox.MusicTab()
 										n = G.UIT.C,
 										config = { align = "cm", minh = 0.8, minw = 4, colour = G.C.GREEN, r = 0.2, emboss = 0.05, button = 'hpot_jukebox_play' },
 										nodes = {
-											create_center_aligned_text{ align = "cm", text = JTJukebox.ActuallyPlaying and "Unrequest Song" or "Request Song", colour = G.C.UI.TEXT_LIGHT, maxw = 3.5, scale = 0.5, shadow = true }
+											create_center_aligned_text { align = "cm", text = localize('hotpot_jukebox_' .. (JTJukebox.ActuallyPlaying and 'un' or '') .. 'request'), colour = G.C.UI.TEXT_LIGHT, maxw = 3.5, scale = 0.5, shadow = true }
 										},
 									},
 									-- go next
@@ -469,7 +498,7 @@ function JTJukebox.MusicTab()
 										n = G.UIT.C,
 										config = { align = "cm", minh = 0.8, minw = 0.8, colour = G.C.RED, r = 0.2, emboss = 0.05, button = 'hpot_jukebox_go_next' },
 										nodes = {
-											create_center_aligned_text{ align = "cm", text = ">", colour = G.C.UI.TEXT_LIGHT, maxw = 0.7, scale = 0.75, shadow = true }
+											create_center_aligned_text { align = "cm", text = ">", colour = G.C.UI.TEXT_LIGHT, maxw = 0.7, scale = 0.75, shadow = true }
 										}
 									},
 								}
