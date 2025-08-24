ed32e44e0e4ac1dc44db56499c12a7dda270fe40
CodeRevo <devrimulasyurtsever00@gmail.com>
2025-08-25 00:04:03 +0300

    Credits in hud

--- Team Name/tname_funcs.lua
+++ Team Name/tname_funcs.lua
@@ -52,14 +52,71 @@ function add_tables(tables) -- yet again, there is probably a better way to do t
 	return ful_tab
 end
 
-function HPTN.ease_credits(amount)
-	G.PROFILES[G.SETTINGS.profile].TNameCredits = G.PROFILES[G.SETTINGS.profile].TNameCredits + amount
+
+
+function HPTN.ease_credits(amount, instant)
+
+  local function _mod(mod)  -- Taken from ease_plincoins()
+        local dollar_UI = G.HUD:get_UIE_by_ID('credits_UI_text')
+        mod = mod or 0
+        local text = '+c.'
+        local col = G.C.BLUE
+        if mod < 0 then
+            text = '-c.'
+            col = G.C.RED
+        end
+
+        G.PROFILES[G.SETTINGS.profile].TNameCredits = G.PROFILES[G.SETTINGS.profile].TNameCredits + amount
+		G.GAME.credits_text = G.PROFILES[G.SETTINGS.profile].TNameCredits
+    
+        dollar_UI.config.object:update()
+        G.HUD:recalculate()
+        --Popup text next to the chips in UI showing number of chips gained/lost
+        attention_text({
+          text = text..tostring(math.abs(mod)),
+          scale = 0.8, 
+          hold = 0.7,
+          cover = dollar_UI.parent,
+          cover_colour = col,
+          align = 'cm',
+          })
+        --Play a chip sound
+        play_sound('coin1')
+    end
+
+	if instant then
+        _mod(amount)
+    else
+        G.E_MANAGER:add_event(Event({
+        trigger = 'immediate',
+        func = function()
+            _mod(amount)
+            return true
+        end
+        }))
+    end
+
 	G:save_progress()
 end
+
+function HPTN.set_credits(amount)
+	G.PROFILES[G.SETTINGS.profile].TNameCredits =  amount
+	G.GAME.credits_text = G.PROFILES[G.SETTINGS.profile].TNameCredits
+end
+
 function HPTN.check_if_enough_credits(cost)
 	local credits = G.PROFILES[G.SETTINGS.profile].TNameCredits
 	if (credits - cost) >= 0 then
 		return true
 	end
 	return false
+end
+
+
+G.FUNCS.credits_UI_set = function(e)
+  local new_chips_text = number_format(G.PROFILES[G.SETTINGS.profile].TNameCredits)
+  if G.GAME.credits_text ~= new_chips_text then
+    e.config.scale = math.min(0.8, scale_number(G.PROFILES[G.SETTINGS.profile].TNameCredits, 1.1))
+    G.GAME.credits_text = new_chips_text
+  end
 end
--- Team Name/tname_hooks.lua
+++ Team Name/tname_hooks.lua
@@ -13,6 +13,7 @@ local igo = Game.init_game_object
 Game.init_game_object = function(self)
 	local ret = igo(self)
 	ret.overclock_timer = 3
+	ret.credits_text = G.PROFILES[G.SETTINGS.profile].TNameCredits
 	return ret
 end
 
