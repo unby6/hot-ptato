ec9b961fc2399287a8dd14aacb0c31e406550371
Jogla <Joglacraft@gmail.com>
2025-08-27 18:59:19 +0200

    Move the credits info to the profile menu

--- Team Name/tname_credits.lua
+++ Team Name/tname_credits.lua
@@ -2,17 +2,9 @@
 function G.UIDEF.tname_credits()
     local save = G.PROFILES[G.SETTINGS.profile]
     save.TNameCredits = (save.TNameCredits or 0)
-    return {n = G.UIT.ROOT, config = {
-					colour = G.C.CLEAR
-				}, nodes = {
-					{n = G.UIT.ROOT, config = {r = 0.1, minw = 8, minh = 6, align = "tm", padding = 0.2, colour = G.C.BLACK}, nodes = {
-                        {n = G.UIT.C, config = {r = 0.1, minw = 8, minh = 6, align = "cm", colour = G.C.CLEAR}, nodes = {
-                            {n = G.UIT.R, config = {r = 0.1,minw = 1,align = "cm", padding = 0.2, colour = G.C.GREY}, nodes = {
-                               {n=G.UIT.T, config={text = "You have:", scale = 0.6, colour = G.C.WHITE, shadow = true}},
-                               {n=G.UIT.T, config={text = "c."..G.PROFILES[G.SETTINGS.profile]["TNameCredits"], scale = 0.6, colour = G.C.PURPLE, shadow = true}},
-                               {n=G.UIT.T, config={text = ".", scale = 0.6, colour = G.C.WHITE, shadow = true}},
-                            }}}
-                        }
-				}}
-            }}
+    return
+    {n = G.UIT.R, config = {r = 0.1,minw = 3.5, minh = 0.8,align = "cm", padding = 0, colour = G.C.BLACK}, nodes = {
+        {n=G.UIT.T, config={text = "You have ", scale = 0.6, colour = G.C.WHITE, shadow = true}},
+        {n=G.UIT.T, config={text = "c."..G.PROFILES[G.SETTINGS.profile]["TNameCredits"], scale = 0.6, colour = G.C.PURPLE, shadow = true}},
+    }}
 end
