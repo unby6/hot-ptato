adcefb5320a661543a7eafff9d0f5ba4d1515df6
CodeRevo <devrimulasyurtsever00@gmail.com>
2025-08-30 02:11:04 +0300

    temporary fix to a reroll crash

--- Team Name/Jokers/tname_creditables.lua
+++ Team Name/Jokers/tname_creditables.lua
@@ -1,46 +1,43 @@
 -- these arent really balanced so feel free to rebalance them :)
-SMODS.Joker:take_ownership('j_joker',
-    {
-    atlas = "teamname_shitfuck",
-    credits = 120,
-    rarity = "hpot_creditable",
+SMODS.Joker:take_ownership("j_joker", {
+	atlas = "teamname_shitfuck",
+	credits = 120,
+	rarity = "hpot_creditable",
 	cost = 0,
-    loc_txt = {name = "Joker", text = {"{C:attention}Revives{} one character"}},
-    loc_vars = function (self, info_queue, card)
-        return { vars = {} }
-    end,
-    pools = {
-        CreditablePool = true
-    },
-    config = {},
-    calculate = function (self, card, context)
-        local ck = math.random(56, 98)
-        if context.end_of_round and context.game_over and context.main_eval then
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    G.hand_text_area.blind_chips:juice_up()
-                    G.hand_text_area.game_chips:juice_up()
-                    play_sound('tarot1')
-                    card:start_dissolve()
-                    return true
-                end -- copied from vr LMAO
-            }))
-            return {
-                message = "+"..ck.." HP",
-                saved = 'teamname_off_reference',
-                colour = G.C.GREEN
-            }
-        end
-    end,
-    hotpot_credits = {
-        art = {'Mortis Ghost'},
-        idea = {"GoldenLeaf"},
-        code = {'GoldenLeaf'},
-        team = {'Team Name'}
-    },
-    },
-    false
-)
+	loc_txt = { name = "Joker", text = { "{C:attention}Revives{} one character" } },
+	loc_vars = function(self, info_queue, card)
+		return { vars = {} }
+	end,
+	pools = {
+		CreditablePool = true,
+	},
+	config = {},
+	calculate = function(self, card, context)
+		local ck = math.random(56, 98)
+		if context.end_of_round and context.game_over and context.main_eval then
+			G.E_MANAGER:add_event(Event({
+				func = function()
+					G.hand_text_area.blind_chips:juice_up()
+					G.hand_text_area.game_chips:juice_up()
+					play_sound("tarot1")
+					card:start_dissolve()
+					return true
+				end, -- copied from vr LMAO
+			}))
+			return {
+				message = "+" .. ck .. " HP",
+				saved = "teamname_off_reference",
+				colour = G.C.GREEN,
+			}
+		end
+	end,
+	hotpot_credits = {
+		art = { "Mortis Ghost" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
+}, false)
 
 SMODS.Joker({
 	key = "grand_finale",
@@ -48,47 +45,47 @@ SMODS.Joker({
 	atlas = "tname_jokers",
 	pos = {
 		x = 0,
-		y = 1
+		y = 1,
 	},
 	cost = 0,
 	credits = 500,
 	config = {
 		extra = {
-			slots = 3
+			slots = 3,
 		},
 	},
-        pools = {
-        CreditablePool = true
-    },
+	pools = {
+		CreditablePool = true,
+	},
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.extra.slots } }
 	end,
-	add_to_deck = function (self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                if G.jokers then
-                    G.jokers.config.card_limit = G.jokers.config.card_limit + card.ability.extra.slots
-                end
-                return true
-            end,
-        }))
-    end,
-	remove_from_deck = function (self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                if G.jokers then
-                    G.jokers.config.card_limit = G.jokers.config.card_limit - card.ability.extra.slots
-                end
-                return true
-            end,
-        }))
+	add_to_deck = function(self, card, from_debuff)
+		G.E_MANAGER:add_event(Event({
+			func = function()
+				if G.jokers then
+					G.jokers.config.card_limit = G.jokers.config.card_limit + card.ability.extra.slots
+				end
+				return true
+			end,
+		}))
 	end,
-    hotpot_credits = {
-        art = {"GoldenLeaf"},
-        idea = {"GoldenLeaf"},
-        code = {"GoldenLeaf"},
-        team = {"Team Name"}
-    }
+	remove_from_deck = function(self, card, from_debuff)
+		G.E_MANAGER:add_event(Event({
+			func = function()
+				if G.jokers then
+					G.jokers.config.card_limit = G.jokers.config.card_limit - card.ability.extra.slots
+				end
+				return true
+			end,
+		}))
+	end,
+	hotpot_credits = {
+		art = { "GoldenLeaf" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
 })
 
 SMODS.Joker({
@@ -97,47 +94,47 @@ SMODS.Joker({
 	atlas = "tname_jokers",
 	pos = {
 		x = 1,
-		y = 1
+		y = 1,
 	},
 	cost = 0,
 	credits = 500,
 	config = {
 		extra = {
-			slots = 2
+			slots = 2,
 		},
 	},
-        pools = {
-        CreditablePool = true
-    },
+	pools = {
+		CreditablePool = true,
+	},
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.extra.slots } }
 	end,
-	add_to_deck = function (self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                if G.consumeables then
-                    G.consumeables.config.card_limit = G.consumeables.config.card_limit + card.ability.extra.slots
-                end
-                return true
-            end,
-        }))
-    end,
-	remove_from_deck = function (self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                if G.consumeables then
-                    G.consumeables.config.card_limit = G.consumeables.config.card_limit - card.ability.extra.slots
-                end
-                return true
-            end,
-        }))
+	add_to_deck = function(self, card, from_debuff)
+		G.E_MANAGER:add_event(Event({
+			func = function()
+				if G.consumeables then
+					G.consumeables.config.card_limit = G.consumeables.config.card_limit + card.ability.extra.slots
+				end
+				return true
+			end,
+		}))
 	end,
-    hotpot_credits = {
-        art = {"GoldenLeaf"},
-        idea = {"GoldenLeaf"},
-        code = {"GoldenLeaf"},
-        team = {"Team Name"}
-    }
+	remove_from_deck = function(self, card, from_debuff)
+		G.E_MANAGER:add_event(Event({
+			func = function()
+				if G.consumeables then
+					G.consumeables.config.card_limit = G.consumeables.config.card_limit - card.ability.extra.slots
+				end
+				return true
+			end,
+		}))
+	end,
+	hotpot_credits = {
+		art = { "GoldenLeaf" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
 })
 
 SMODS.Joker({
@@ -146,39 +143,39 @@ SMODS.Joker({
 	atlas = "tname_jokers",
 	pos = {
 		x = 2,
-		y = 1
+		y = 1,
 	},
-        pools = {
-        CreditablePool = true
-    },
-	add_to_deck = function (self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
+	pools = {
+		CreditablePool = true,
+	},
+	add_to_deck = function(self, card, from_debuff)
+		G.E_MANAGER:add_event(Event({
+			func = function()
 				local a = G.GAME.tarot_rate
-                G.GAME.tarot_rate = G.GAME.spectral_rate
+				G.GAME.tarot_rate = G.GAME.spectral_rate
 				G.GAME.spectral_rate = a
-                return true
-            end,
-        }))
-    end,
-	remove_from_deck = function (self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
+				return true
+			end,
+		}))
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		G.E_MANAGER:add_event(Event({
+			func = function()
 				local a = G.GAME.tarot_rate
-                G.GAME.tarot_rate = G.GAME.spectral_rate
+				G.GAME.tarot_rate = G.GAME.spectral_rate
 				G.GAME.spectral_rate = a
-                return true
-            end,
-        }))
+				return true
+			end,
+		}))
 	end,
 	cost = 0,
 	credits = 500,
-    hotpot_credits = {
-        art = {"GoldenLeaf"},
-        idea = {"GoldenLeaf"},
-        code = {"GoldenLeaf"},
-        team = {"Team Name"}
-    }
+	hotpot_credits = {
+		art = { "GoldenLeaf" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
 })
 
 SMODS.Joker({
@@ -187,40 +184,40 @@ SMODS.Joker({
 	atlas = "tname_jokers",
 	pos = {
 		x = 0,
-		y = 2
+		y = 2,
 	},
-        pools = {
-        CreditablePool = true
-    },
+	pools = {
+		CreditablePool = true,
+	},
 	cost = 0,
 	credits = 500,
-    calculate = function(self, card, context)
+	calculate = function(self, card, context)
 		local ret = SMODS.blueprint_effect(card, G.jokers.cards[1], context)
 		local ret2 = SMODS.blueprint_effect(card, G.jokers.cards[#G.jokers.cards], context)
 		local etr = nil
-        if ret then
-            if ret2 then
-                ret.extra = ret2 
-            end
-        etr = ret
-        elseif ret2 then
-            etr = ret2
-        else
-            etr = nil 
-        end
+		if ret then
+			if ret2 then
+				ret.extra = ret2
+			end
+			etr = ret
+		elseif ret2 then
+			etr = ret2
+		else
+			etr = nil
+		end
 
-    if etr then
-        return etr 
-    else
-        return nil, true
-    end
-    end,
-    hotpot_credits = {
-        art = {"GoldenLeaf"},
-        idea = {"GoldenLeaf"},
-        code = {"GoldenLeaf"},
-        team = {"Team Name"}
-    }
+		if etr then
+			return etr
+		else
+			return nil, true
+		end
+	end,
+	hotpot_credits = {
+		art = { "GoldenLeaf" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
 })
 
 SMODS.Joker({
@@ -229,34 +226,34 @@ SMODS.Joker({
 	atlas = "tname_jokers",
 	pos = {
 		x = 1,
-		y = 2
+		y = 2,
 	},
 	cost = 0,
 	credits = 500,
 	config = {
 		extra = {
-			xmult = 7
+			xmult = 7,
 		},
 	},
-    pools = {
-        CreditablePool = true
-    },
+	pools = {
+		CreditablePool = true,
+	},
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.extra.xmult } }
 	end,
 	calculate = function(self, card, context)
-        if context.joker_main then
-            return {
-                xmult = card.ability.extra.xmult
-            }
-        end
-    end,
-    hotpot_credits = {
-        art = {"GoldenLeaf"},
-        idea = {"GoldenLeaf"},
-        code = {"GoldenLeaf"},
-        team = {"Team Name"}
-    }
+		if context.joker_main then
+			return {
+				xmult = card.ability.extra.xmult,
+			}
+		end
+	end,
+	hotpot_credits = {
+		art = { "GoldenLeaf" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
 })
 
 SMODS.Joker({
@@ -266,71 +263,76 @@ SMODS.Joker({
 	atlas = "tname_jokers",
 	pos = {
 		x = 2,
-		y = 0
+		y = 0,
 	},
-        pools = {
-        CreditablePool = true
-    },
+	pools = {
+		CreditablePool = true,
+	},
 	credits = 5000,
-	calculate = function (self, card, context)
-		if G.GAME.blind.config.blind and G.GAME.blind.config.blind.boss and G.GAME.blind.config.blind.boss.showdown and HPTN.is_shitfuck then
-        G.E_MANAGER:add_event(Event({
-            trigger = 'before',
-            delay = 0.4,
-            func = function()
-                for _, joker in pairs(G.jokers.cards) do
-                        joker:start_dissolve(nil, true)
-                end
-                return true
-            end
-        }))
-		G.E_MANAGER:add_event(Event({
-            trigger = 'after',
-            delay = 0.4,
-            func = function()
-                SMODS.add_card({key = "j_hpot_space_ape"})
-                SMODS.add_card({key = "j_hpot_space_ape"})
-                SMODS.add_card({key = "j_hpot_space_ape"})
-                SMODS.add_card({key = "j_hpot_space_ape"})
-                SMODS.add_card({key = "j_hpot_space_ape"})
-                return true
-            end
-        }))
-		HPTN.is_shitfuck = false
+	calculate = function(self, card, context)
+		if
+			G.GAME.blind.config.blind
+			and G.GAME.blind.config.blind.boss
+			and G.GAME.blind.config.blind.boss.showdown
+			and HPTN.is_shitfuck
+		then
+			G.E_MANAGER:add_event(Event({
+				trigger = "before",
+				delay = 0.4,
+				func = function()
+					for _, joker in pairs(G.jokers.cards) do
+						joker:start_dissolve(nil, true)
+					end
+					return true
+				end,
+			}))
+			G.E_MANAGER:add_event(Event({
+				trigger = "after",
+				delay = 0.4,
+				func = function()
+					SMODS.add_card({ key = "j_hpot_space_ape" })
+					SMODS.add_card({ key = "j_hpot_space_ape" })
+					SMODS.add_card({ key = "j_hpot_space_ape" })
+					SMODS.add_card({ key = "j_hpot_space_ape" })
+					SMODS.add_card({ key = "j_hpot_space_ape" })
+					return true
+				end,
+			}))
+			HPTN.is_shitfuck = false
 		end
 	end,
-    hotpot_credits = {
-        art = {"GoldenLeaf"},
-        idea = {"GoldenLeaf"},
-        code = {"GoldenLeaf"},
-        team = {"Team Name"}
-    }
+	hotpot_credits = {
+		art = { "GoldenLeaf" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
 })
-
 
 SMODS.Joker({
 	key = "space_ape",
 	atlas = "tname_jokers",
 	pos = {
 		x = 1,
-		y = 0
+		y = 0,
 	},
-        pools = {
-        CreditablePool = true
-    },
+	pools = {
+		CreditablePool = true,
+	},
 	rarity = "hpot_creditable",
 	cost = 0,
 	credits = 0,
 	no_collection = true,
-	in_pool = function(self, args) return false end,
-	add_to_deck = function (self, card, from_debuff)
-        SMODS.Stickers["eternal"]:apply(card, true)
-    end,
-    hotpot_credits = {
-        art = {"Mortis Ghost"},
-        idea = {"GoldenLeaf"},
-        code = {"GoldenLeaf"},
-        team = {"Team Name"}
-    }
+	in_pool = function(self, args)
+		return false
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		SMODS.Stickers["eternal"]:apply(card, true)
+	end,
+	hotpot_credits = {
+		art = { "Mortis Ghost" },
+		idea = { "GoldenLeaf" },
+		code = { "GoldenLeaf" },
+		team = { "Team Name" },
+	},
 })
-
