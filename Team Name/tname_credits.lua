d5b7d76203c753abd3b6332dc2f7d3a87053b9e6
CodeRevo <devrimulasyurtsever00@gmail.com>
2025-08-30 04:27:18 +0300

    number_format() for the profile credits thing

--- Team Name/tname_credits.lua
+++ Team Name/tname_credits.lua
@@ -5,6 +5,6 @@ function G.UIDEF.tname_credits()
     return
     {n = G.UIT.R, config = {r = 0.1,minw = 3.5, minh = 0.8,align = "cm", padding = 0, colour = G.C.BLACK}, nodes = {
         {n=G.UIT.T, config={text = localize("you_have"), scale = 0.6, colour = G.C.WHITE, shadow = true}},
-        {n=G.UIT.T, config={text = "c."..G.PROFILES[HPTN.Profile]["TNameCredits"], scale = 0.6, colour = G.C.PURPLE, shadow = true}},
+        {n=G.UIT.T, config={text = "c."..number_format(G.PROFILES[HPTN.Profile]["TNameCredits"]), scale = 0.6, colour = G.C.PURPLE, shadow = true}},
     }}
 end
