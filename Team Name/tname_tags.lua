672810832a6a70905f11ebe37ed18dbe7b437e4c
CodeRevo <devrimulasyurtsever00@gmail.com>
2025-08-30 16:49:29 +0300

    psychedelic tag

--- Team Name/tname_tags.lua
+++ Team Name/tname_tags.lua
@@ -125,3 +125,37 @@ SMODS.Tag({
 		team = { "Team Name" },
 	},
 })
+
+
+SMODS.Tag {
+    key = "psychedelic_tag",
+    min_ante = 2,
+    loc_vars = function(self, info_queue, tag)
+        info_queue[#info_queue + 1] = G.P_CENTERS.e_hpot_psychedelic
+    end,
+    apply = function(self, tag, context)
+        if context.type == 'store_joker_modify' then
+            if not context.card.edition and not context.card.temp_edition and context.card.ability.set == 'Joker' then
+                local lock = tag.ID
+                G.CONTROLLER.locks[lock] = true
+                context.card.temp_edition = true
+                tag:yep('+', G.C.DARK_EDITION, function()
+                    context.card.temp_edition = nil
+                    context.card:set_edition("e_hpot_psychedelic", true)
+                    context.card.ability.couponed = true
+                    context.card:set_cost()
+                    G.CONTROLLER.locks[lock] = nil
+                    return true
+                end)
+                tag.triggered = true
+                return true
+            end
+        end
+    end,
+	hotpot_credits = {
+		art = { "GoldeLeaf" },
+		idea = { "Revo" },
+		code = { "Revo" },
+		team = { "Team Name" },
+	},
+}
