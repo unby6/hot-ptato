38716a4bf0612448a77c5ddf1c946c88f6903b6d
Ruby <darkdragongaming379@gmail.com>
2025-09-08 01:48:39 +0100

    split up name text

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -932,7 +932,11 @@ return {
                 }
             },
             j_hpot_prosopagnosia = {
-                name = "Mr. Joker Encounters a Secondhand Vanity: Tulpamancers Prosopagnosia",
+                name = {
+                    "Mr. Joker Encounters",
+                    "a Secondhand Vanity:",
+                    "Tulpamancers Prosopagnosia"
+                },
                 text = {
                     "{C:attention}Face{} cards instead score as",
                     "as a previously {C:money}sold {C:attention}Joker",
