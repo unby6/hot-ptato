d774e71d0644a91913b38b7961cea150db983afa
cg223 <cg223celeste@gmail.com>
2025-09-09 19:40:25 -0400

    fix compat

--- Horsechicot/jokers/yapper.lua
+++ Horsechicot/jokers/yapper.lua
@@ -10,6 +10,9 @@ SMODS.Joker {
     loc_vars = function (self, info_queue, card)
         return {vars = { card.ability.amt, card.ability.current or HPJTTT.text[1], string.len(card.ability.current) * card.ability.amt}}
     end,
+    blueprint_compat = true,
+    perishable_compat = true,
+    eternal_compat = true,
     calculate = function (self, card, context)
         if context.after then
             card.ability.current = pseudorandom_element(HPJTTT.text, "hc_yapper")
