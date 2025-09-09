86e2dfaf5a199a37752d356d5469ff2150fe8b03
Clicky- <84652396+Clickseee@users.noreply.github.com>
2025-09-09 23:33:01 +0700

    add: joker

---
+++ Horsechicot/blunderfarming.lua
@@ -0,0 +1,26 @@
+SMODS.Joker {
+    key = "blunderfarming",
+    atlas = "hc_placeholder",
+    pos = { x = 0, y = 0 },
+    rarity = 2,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    cost = 4,
+    config = { extra = { odds = 2 } },
+    loc_vars = function(self, info_queue, card)
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'blunderfarming')
+        return { vars = { numerator, denominator } }
+    end
+}
+
+local original_add_tag = add_tag
+function add_tag(tag, immediate)
+    if next(SMODS.find_card("j_hpot_blunderfarming")) and G.GAME.skipping_blind then
+        if pseudorandom('blunder', 1, 2) == 1 then
+                original_add_tag(Tag(tag.key))
+            return
+        end
+    end
+    original_add_tag(tag, immediate)
+end
