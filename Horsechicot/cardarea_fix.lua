ffbcc89ebbf1625449fcc890d5aac8802e98e7a4
cg223 <cg223celeste@gmail.com>
2025-09-12 22:19:16 -0400

    cardarea_fix comment

--- Horsechicot/cardarea_fix.lua
+++ Horsechicot/cardarea_fix.lua
@@ -1,3 +1,4 @@
+--i believe this imporved performance? i think handy had a bug where everything renders regardless. this is almost certainly a reimplementation of a vanilla optimization
 local old = CardArea.draw
 function CardArea:draw(...)
     if not ((self.T.x >= G.ROOM.T.w) or (self.T.y >= G.ROOM.T.h)) then
