a3739873237dae22e38e6f89c29a5d2be2864033
Clicky- <84652396+Clickseee@users.noreply.github.com>
2025-09-13 18:28:56 +0700

    add: joker

---
+++ Horsechicot/jokers/cardstack.lua
@@ -0,0 +1,31 @@
+SMODS.Joker {
+    key = "cardstack",
+    atlas = "hc_jokers",
+    pos = { x = 1, y = 3 },
+    rarity = 2,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    cost = 6,
+    config = { extra = { fuckface = 1, numerator = 1, denumerator = 3 } },
+    loc_vars = function(self, info_queue, card)
+        local numerator, denumerator = SMODS.get_probability_vars(card, card.ability.extra.denumerator, card.ability.extra.numerator, 'bruh')
+        return {
+            vars = { card.ability.extra.fuckface, numerator, denumerator }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and not context.blueprint then
+            if not next(SMODS.get_enhancements(context.other_card)) and SMODS.pseudorandom_probability(card, 'meth', card.ability.extra.numerator, card.ability.extra.denumerator) then
+                context.other_card.ability.perma_repetitions = (context.other_card.ability.perma_repetitions or 0) +
+                card.ability.extra.mario
+                return {
+                    message = localize('k_upgrade_ex'),
+                    card = card
+                }
+            end
+        end
+    end
+}
+
+-- the EXACT same code from Tangents i know
