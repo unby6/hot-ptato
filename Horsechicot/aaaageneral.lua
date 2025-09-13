ac4a04f85e2ca3fd37bf2af2ea10d011ad5a085b
cg223 <cg223celeste@gmail.com>
2025-09-12 21:38:29 -0400

    fix horsechicot quips

--- Horsechicot/aaaageneral.lua
+++ Horsechicot/aaaageneral.lua
@@ -33,68 +33,7 @@ SMODS.Atlas{key = "hc_consumables", path = "Horsechicot/hc_consumables.png", px=
 
 local Username = G.PROFILES[G.SETTINGS.profile].name
 
-HPJTTT.add_texts({
-    "and now... a word from our Sponsor. Entropy.",
-    "Attempt to compare Joker with Table",
-    "Now in 3D",
-    TMJ and "Thanks for using Too Many Jokers! ~ cg" or "Use Too Many Jokers! ~ cg", --tmj prio is lower
-    "Cassoosted Fuper",
-    "Splipped Droost",
-    "mb6fbhsphdrcb",
-    "Spinner Stunning",
-    "JTA",
-    "Bubsdrop",
-    "BIS",
-    "Backboost",
-    "silly ahh drivables - @lily.felli",
-    "130 lbs",
-    "Niko: She's 10. Can I Mate?",
-    CardPronouns and "This is so woke!" or "Uhh.... install CardPronouns!", --cprns prio is  literally -99 million :3
-    "awawa awebo",
-    "I wish shadows had rights",
-    "The cat atop the far tree whispers sweet promises to passerbys",
-    "I think we should stop having money. That sounds like a good thing.",
-    "One or two extra currencies",
-    "Shoutouts to gay foxgirls",
-    "In a Cargo Box?",
-    Username .. ", Stay Determined!",
-    "It's over, " .. Username .. " knows.",
-    "Two Hotties, One potato.",
-    "-- TODO: your mom",
-    Entropy and "You are not immune to Propaganda" or "generic message", --holes bad? 
-    "The Oldest Anarchy Mod In Balatro",
-    "D1D7",
-    "smots gaming",
-    "Tip number 10: Sit at the chessboard and play with yourself",
-    love.system.getOS() .. " is the worst operating system!",
-	"Murph will do anything but wheel of mods",
-	"Grows offline!",
-    "Restraining Order means nothing to me",
-    "Why so serious?",
-    "Why so genius?",
-    CardPronouns and "Why so queerious?" or "generic message",
-    "MEDIC!",
-    "REF DO SOMETHING",
-    "77 + 33 = 100",
-    "Are you guys going trick or treating?",
-    "I'm sorry, but as an AI language model, I don't have consciousness or self-awareness. I'm simply a program designed to process and generate human-like text.",
-    "In case of an investigation by any federal entity or similar, I do not have any involvement with this group or with the people in it, I do not know how I am here, probably added by a thrid party, I do not support any actions by the member of this group.",
-    "Now gluten-free!",
-    "THEIR PRONOUNS ARE THEY/THE",
-    "m",
-    "Made by: The F@#! Squad",
-    "It's never Lupus.",
-    "THIS IS SPARTA!",
-    "I got freaked",
-    "Oh my god bruh",
-    "I can't believe that " .. Username .. " making me do this..",
-    "Okie-dokie Artichokie!",
-    "Numberslop in MY Balatro Mod?! There's more likely than what you think!",
-    "GAY EDITION",
-    "Now compatible with Birthlatro!",
-    "What the dog doin?",
-    "Nerf what if there was a joker",
-})
+
 
 function Horsechicot.get_authors()
     if Horsechicot.known_authors then
@@ -238,12 +177,6 @@ function Horsechicot.post_load()
             G.E_MANAGER:add_event(Event{func = function() G.round_end_lock = false return true end})
         end
     end
-
-    for i, v in pairs(Horsechicot.get_authors()) do
-        HPJTTT.add_texts{
-            "Contributor shoutout: " .. v
-        }
-    end
 end
 
 
@@ -266,5 +199,3 @@ SMODS.Atlas{key = "hcbananaad", path = "Ads/BananaAd.png", px=169, py=55}
 SMODS.Atlas{key = "hcnumberslop", path = "Ads/numberslop.png", px=169, py=55}
 SMODS.Atlas{key = "hcred", path = "Ads/red.png", px=169, py=55}
 SMODS.Atlas{key = "hcmustard", path = "Ads/mustard.png", px=169, py=55}
-
-HPJTTT.chosen = (math.floor(os.time())%#HPJTTT.text)+1
