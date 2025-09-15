8c4d5714460234c337095413d131c7dbcc17734d
the-Astra <djs8743@rit.edu>
2025-09-14 21:21:03 -0400

    I forgot Ouroboros joker_main whoops

--- Oops All Programmers/Jokers/ouroboros.lua
+++ Oops All Programmers/Jokers/ouroboros.lua
@@ -18,27 +18,33 @@ SMODS.Joker {
             local eligible_cards = {}
 
             for k, v in pairs(G.jokers.cards) do
-                if v ~= card and v~= context.card then
-                    eligible_cards[#eligible_cards+1] = v
+                if v ~= card and v ~= context.card then
+                    eligible_cards[#eligible_cards + 1] = v
                 end
             end
 
             local destroy_card = pseudorandom_element(eligible_cards)
             destroy_card:start_dissolve()
 
-            SMODS.scale_card(card,{
+            SMODS.scale_card(card, {
                 ref_table = card.ability.extra,
                 ref_value = 'Xmult',
                 scalar_value = 'gain',
                 message_colour = G.C.MULT
             })
         end
+
+        if context.joker_main and card.ability.extra.Xmult > 1 then
+            return {
+                xmult = card.ability.extra.Xmult
+            }
+        end
     end,
     hotpot_credits = {
-        art = {'?'},
-        code = {'theAstra'},
-        idea = {'the30ne'},
-        team = {'Oops! All Programmers'}
+        art = { '?' },
+        code = { 'theAstra' },
+        idea = { 'the30ne' },
+        team = { 'Oops! All Programmers' }
     }
 }
 
