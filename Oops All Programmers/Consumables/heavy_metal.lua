f0abe22d0dee9170129f31d2e98b9cf659396ac6
the-Astra <djs8743@rit.edu>
2025-09-17 15:16:50 -0400

    Heavy Metal credits

--- Oops All Programmers/Consumables/heavy_metal.lua
+++ Oops All Programmers/Consumables/heavy_metal.lua
@@ -6,5 +6,11 @@ SMODS.Consumable {
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue + 1] = G.P_CENTERS[card.ability.mod_conv]
         return { vars = { card.ability.max_highlighted, localize { type = 'name_text', set = 'Enhanced', key = card.ability.mod_conv } } }
-    end
+    end,
+    hotpot_credits = {
+        art = { 'SadCube' },
+        code = { 'theAstra' },
+        idea = { 'wix', 'SadCube' },
+        team = { 'Oops! All Programmers' }
+    }
 }
