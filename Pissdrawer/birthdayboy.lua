71d7e5d319b6b5817fcad6531b1e15d8bc2ea002
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-09-20 15:27:21 -0400

    i made exactly one change to the title screen

---
+++ Pissdrawer/birthdayboy.lua
@@ -0,0 +1,15 @@
+SMODS.Atlas { key = "birthdayboy", path = "Pissdrawer/pdr_birthdayboy.png", px = 71, py = 95 }
+SMODS.Joker {
+    key = "birthdayboy",
+    loc_txt = {
+        name = "Birthday Boy",
+        text = { "Happy Birthday, N'!" }
+    },
+    atlas = "birthdayboy",
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 1, y = 0 },
+    unlocked = true,
+    discovered = true,
+    rarity = 4,
+    no_collection = true
+}
