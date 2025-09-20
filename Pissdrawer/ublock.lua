725764a11bc49bceff447e97af0cb72972e9a7fa
fey-fish <tmarshall1309@gmail.com>
2025-09-20 23:17:35 +0100

    modified:   Pissdrawer/ublock.lua

--- Pissdrawer/ublock.lua
+++ Pissdrawer/ublock.lua
@@ -1,21 +1,11 @@
 SMODS.Back {
     key = 'ublockdeck',
-    discovered = true,
-    apply = function(self, back)
-        G.E_MANAGER:add_event(Event({
-            trigger = 'after',
-            delay = 3,
-            func = function()
-                for k, v in ipairs(G.GAME.hotpot_ads) do
-                    G.E_MANAGER:add_event(Event({
-                        func = function()
-                            G.FUNCS.remove_ad(v.config.id)
-                            return true
-                        end
-                    }))
-                end
-                return true
-            end
-        }))
-    end
+    discovered = true
 }
+
+local ref = create_ads
+function create_ads(e)
+    if G.GAME.selected_back.name ~= 'b_hpot_ublockdeck' then
+        return ref(e)
+    end
+end
