7f2d8f5ce7a4be021808894ddeff1cbbaee4b84c
SDM0 <60699143+SDM0@users.noreply.github.com>
2025-09-21 13:39:48 +0200

    Scrapped "Hardcore Mode", Added "Minimum Prize Guarantee"

--- Pissdrawer/jokers.lua
+++ Pissdrawer/jokers.lua
@@ -1,5 +1,5 @@
 SMODS.Joker {
-    key = 'hardcore_mode',
+    key = 'minimum_prize_guarantee',
     rarity = 1,
     cost = 5,
     atlas = "pdr_joker",
@@ -11,35 +11,27 @@ SMODS.Joker {
         team = { 'Pissdrawer' }
     },
     calculate = function(self, card, context)
-        if context.using_consumeable and context.area == G.plinko_rewards and not (context.consumeable.edition and context.consumeable.edition.negative) then
+        if context.using_consumeable and context.area == G.plinko_rewards and context.consumeable.edition and context.consumeable.edition.negative then
             if G.GAME.current_round and G.GAME.current_round.plinko_preroll_cost and G.GAME.current_round.plinko_preroll_cost ~= 0 then
-                ease_plincoins(G.GAME.current_round.plinko_preroll_cost)
+                if #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                    G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                    G.E_MANAGER:add_event(Event({
+                        func = (function()
+                            SMODS.add_card {
+                                set = 'Czech',
+                                key_append = 'j_hpot_minimum_prize_guarantee'
+                            }
+                            G.GAME.consumeable_buffer = 0
+                            return true
+                        end)
+                    }))
+                    return {
+                        message = localize('k_plus_czech'),
+                        colour = G.C.SECONDARY_SET.Czech,
+                    }
+                end
             end
         end
         return nil, true
     end,
-}
-
-SMODS.Joker {
-    key = 'pwiz',
-    loc_txt = {name = 'Piss Wizard', text = {'{C:money}Piss{} escaped the drawer :/'}},
-    rarity = 1,
-    cost = 5,
-    atlas = "pdr_joker",
-    pos = { x = 0, y = 0 },
-    hotpot_credits = {
-        art = { 'SDM_0' },
-        code = { 'fey <3' },
-        idea = { 'fey <3' },
-        team = { 'Pissdrawer' }
-    },
-    add_to_deck = function(self,card)
-        for i,v in pairs(G.C) do
-            if (type(G.C[i][1]) == 'number') or SMODS.Gradient[i] then G.C[i] = HEX('DCBA1E') end
-        end
-    end,
-    no_collection = true,
-    in_pool = function(self, args)
-        return false
-    end
 }
