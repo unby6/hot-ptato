ca68e90b5292e5eb51b2421232f194869d2f5a58
fey-fish <tmarshall1309@gmail.com>
2025-09-21 01:37:46 +0100

    modified:   Pissdrawer/jokers.lua

--- Pissdrawer/jokers.lua
+++ Pissdrawer/jokers.lua
@@ -19,3 +19,27 @@ SMODS.Joker {
         return nil, true
     end,
 }
+
+SMODS.Joker {
+    key = 'pwiz',
+    loc_txt = {name = 'Piss Wizard', text = {'{C:money}Piss{} escaped the drawer :/'}},
+    rarity = 1,
+    cost = 5,
+    atlas = "pdr_joker",
+    pos = { x = 0, y = 0 },
+    hotpot_credits = {
+        art = { 'SDM_0' },
+        code = { 'fey <3' },
+        idea = { 'fey <3' },
+        team = { 'Pissdrawer' }
+    },
+    add_to_deck = function(self,card)
+        for i,v in pairs(G.C) do
+            if (type(G.C[i][1]) == 'number') or SMODS.Gradient[i] then G.C[i] = HEX('DCBA1E') end
+        end
+    end,
+    no_collection = true,
+    in_pool = function(self, args)
+        return false
+    end
+}
