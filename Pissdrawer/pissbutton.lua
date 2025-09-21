c80b0008e549befc70afa3bec0df287c5003b96e
fey-fish <tmarshall1309@gmail.com>
2025-09-21 20:03:13 +0100

    modified:   Pissdrawer/pissbutton.lua

--- Pissdrawer/pissbutton.lua
+++ Pissdrawer/pissbutton.lua
@@ -2,11 +2,8 @@ bigButtonBirthdayBash = {}
 local peeButtRef = UIBox.init
 function UIBox:init(args)
     local ret = peeButtRef(self, args)
-    local pissbutton = G.UIDEF.pissbutton()
-    if ret then
-        if ret[definition] == create_UIBox_buttons() then
-            table.insert(ret, pissbutton)
-        end
+    if args.config.button then
+        table.insert(bigButtonBirthdayBash, ret)
     end
     return ret
 end
