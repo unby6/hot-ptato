31dd38fadf093c5f140a11cdb6c312501826cba0
Sebastián Venturino <venturinosebastian@gmail.com>
2025-09-22 22:49:13 -0300

    add info queue to tags

--- Team Name/tname_tags.lua
+++ Team Name/tname_tags.lua
@@ -1,8 +1,8 @@
 SMODS.Tag({
 	key = "credits_tag",
 	config = { add = 15 },
-    atlas = "tname_tags",
-    pos = {x = 3, y = 0},
+	atlas = "tname_tags",
+	pos = { x = 3, y = 0 },
 	loc_vars = function(self, info_queue, tag)
 		return { vars = { tag.config.add } }
 	end,
@@ -25,8 +25,11 @@ SMODS.Tag({
 
 SMODS.Tag({
 	key = "mega_hanafuda",
-    atlas = "tname_tags",
-    pos = {x = 1, y = 0},
+	atlas = "tname_tags",
+	pos = { x = 1, y = 0 },
+	loc_vars = function(self, info_queue, tag)
+		info_queue[#info_queue + 1] = G.P_CENTERS.p_hpot_hanafuda_mega_1
+	end,
 	apply = function(self, tag, context)
 		if context.type == "new_blind_choice" then
 			tag:yep("+", G.C.SECONDARY_SET.Spectral, function()
@@ -61,8 +64,11 @@ SMODS.Tag({
 
 SMODS.Tag({
 	key = "mega_auras",
-    atlas = "tname_tags",
-    pos = {x = 0, y = 0},
+	atlas = "tname_tags",
+	pos = { x = 0, y = 0 },
+	loc_vars = function(self, info_queue, tag)
+		info_queue[#info_queue + 1] = G.P_CENTERS.p_hpot_auras_mega_1
+	end,
 	apply = function(self, tag, context)
 		if context.type == "new_blind_choice" then
 			tag:yep("+", G.C.SECONDARY_SET.Spectral, function()
@@ -97,8 +103,8 @@ SMODS.Tag({
 
 SMODS.Tag({
 	key = "credit_econ",
-    atlas = "tname_tags",
-    pos = {x = 2, y = 0},
+	atlas = "tname_tags",
+	pos = { x = 2, y = 0 },
 	config = { max = 25 },
 	loc_vars = function(self, info_queue, tag)
 		return { vars = { tag.config.max } }
@@ -107,12 +113,12 @@ SMODS.Tag({
 		if context.type == "immediate" then
 			tag:yep("+", G.C.PURPLE, function()
 				G.E_MANAGER:add_event(Event({
-                    trigger = 'immediate',
-                    func = function()
-                        HPTN.ease_credits(math.min(tag.config.max, math.max(0,G.GAME.credits_text)), true)
-                        return true
-                    end
-                }))
+					trigger = 'immediate',
+					func = function()
+						HPTN.ease_credits(math.min(tag.config.max, math.max(0, G.GAME.credits_text)), true)
+						return true
+					end
+				}))
 				return true
 			end)
 			tag.triggered = true
@@ -128,32 +134,32 @@ SMODS.Tag({
 
 
 SMODS.Tag {
-    key = "psychedelic_tag",
-    min_ante = 2,
+	key = "psychedelic_tag",
+	min_ante = 2,
 	atlas = "tname_tags",
-    pos = {x = 4, y = 0},
-    loc_vars = function(self, info_queue, tag)
-        info_queue[#info_queue + 1] = G.P_CENTERS.e_hpot_psychedelic
-    end,
-    apply = function(self, tag, context)
-        if context.type == 'store_joker_modify' then
-            if not context.card.edition and not context.card.temp_edition and context.card.ability.set == 'Joker' then
-                local lock = tag.ID
-                G.CONTROLLER.locks[lock] = true
-                context.card.temp_edition = true
-                tag:yep('+', G.C.DARK_EDITION, function()
-                    context.card.temp_edition = nil
-                    context.card:set_edition("e_hpot_psychedelic", true)
-                    context.card.ability.couponed = true
-                    context.card:set_cost()
-                    G.CONTROLLER.locks[lock] = nil
-                    return true
-                end)
-                tag.triggered = true
-                return true
-            end
-        end
-    end,
+	pos = { x = 4, y = 0 },
+	loc_vars = function(self, info_queue, tag)
+		info_queue[#info_queue + 1] = G.P_CENTERS.e_hpot_psychedelic
+	end,
+	apply = function(self, tag, context)
+		if context.type == 'store_joker_modify' then
+			if not context.card.edition and not context.card.temp_edition and context.card.ability.set == 'Joker' then
+				local lock = tag.ID
+				G.CONTROLLER.locks[lock] = true
+				context.card.temp_edition = true
+				tag:yep('+', G.C.DARK_EDITION, function()
+					context.card.temp_edition = nil
+					context.card:set_edition("e_hpot_psychedelic", true)
+					context.card.ability.couponed = true
+					context.card:set_cost()
+					G.CONTROLLER.locks[lock] = nil
+					return true
+				end)
+				tag.triggered = true
+				return true
+			end
+		end
+	end,
 	hotpot_credits = {
 		art = { "GoldeLeaf" },
 		idea = { "Revo" },
