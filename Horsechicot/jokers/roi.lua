a25279f31d4783ee3b3e7614c20aff849bdfa825
cg223 <cg223celeste@gmail.com>
2025-09-27 12:08:59 -0400

    fix roi to not give money on failed purchase

--- Horsechicot/jokers/roi.lua
+++ Horsechicot/jokers/roi.lua
@@ -2,7 +2,8 @@ local old = G.FUNCS.buy_from_shop
 function G.FUNCS.buy_from_shop(e)
     local card = e.config.ref_table
     local event
-    if Object.is(card, Card) then
+    local ret = old(e)
+    if ret and Object.is(card, Card) then
         if card.area == G.market_jokers and not G.GAME.bm_bought_this_round then
             for i, v in pairs(SMODS.find_card("j_hpot_roi")) do
                 G.GAME.bm_bought_this_round = true
@@ -14,7 +15,6 @@ function G.FUNCS.buy_from_shop(e)
             end
         end
     end
-    local ret = old(e)
     if event then
         G.E_MANAGER:add_event(event)
     end
