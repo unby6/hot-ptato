a8284f9d5ff50d56c6c55495abdd4595b4d4ae7a
cg223 <cg223celeste@gmail.com>
2025-09-27 19:44:36 -0400

    kill drugs

--- Horsechicot/drugs/_main.lua
+++
@@ -1,7 +0,0 @@
-Horsechicot.drug_primary = HEX("FFFFFF") --todo: make actual colors
-Horsechicot.drug_secondary = HEX("FFFFFF")
--- SMODS.ConsumableType {
---     key = "drug",
---     primary_colour = Horsechicot.drug_primary,
---     secondary_colour = Horsechicot.drug_secondary
--- }
