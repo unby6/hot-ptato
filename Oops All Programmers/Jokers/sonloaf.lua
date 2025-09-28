6c60fe756415f9783501c2d803c4f73534138601
Ruby <darkdragongaming379@gmail.com>
2025-09-28 11:49:48 +0100

    add nil check to son loaf

--- Oops All Programmers/Jokers/sonloaf.lua
+++ Oops All Programmers/Jokers/sonloaf.lua
@@ -12,7 +12,7 @@ SMODS.Joker {
         info_queue[#info_queue+1] = { key = "hpot_sonloaf_comment", set = "Other" } -- remove after sprite pls - trif
     end,
     calculate = function(self, card, context)
-        if context.joker_type_destroyed and context.card.config.center.pools.Food and G.jokers.config.card_limit - (#G.jokers.cards + G.GAME.joker_buffer) then
+        if context.joker_type_destroyed and context.card.config.center.pools and context.card.config.center.pools.Food and G.jokers.config.card_limit - (#G.jokers.cards + G.GAME.joker_buffer) then
             G.E_MANAGER:add_event(Event({
                 func = function()
                     SMODS.add_card {
