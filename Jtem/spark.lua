b87bd85ebec67dd737c1138eaffc27e36566c76b
makatymba2001 <makatymba2001@yandex.ru>
2025-09-28 14:42:30 +0300

    Hide spark points from hud

--- Jtem/spark.lua
+++ Jtem/spark.lua
@@ -2,7 +2,7 @@
 
 function ease_spark_points(mod, instant)
 	local function _mod(mod)
-		local dollar_UI = G.HUD:get_UIE_by_ID('spark_text_UI')
+		local dollar_UI = G.HUD:get_UIE_by_ID('dollar_text_UI')
 		mod = mod or 0
 		local text = '+'
 		local col = G.C.BLUE
@@ -18,9 +18,9 @@ function ease_spark_points(mod, instant)
 		--Popup text next to the chips in UI showing number of chips gained/lost
 		attention_text({
 			text = text .. tostring(math.abs(mod)),
-			scale = 0.8*0.4,
+			scale = 0.8,
 			hold = 0.7,
-			cover = dollar_UI,
+			cover = dollar_UI.parent,
 			cover_colour = col,
 			align = 'cm',
 		})
@@ -40,89 +40,89 @@ function ease_spark_points(mod, instant)
 	end
 end
 
-local c_u_h_ref = create_UIBox_HUD
-function create_UIBox_HUD()
-	local nodes = c_u_h_ref()
+-- local c_u_h_ref = create_UIBox_HUD
+-- function create_UIBox_HUD()
+-- 	local nodes = c_u_h_ref()
 
-	--again, why are u hooking and not patching like everyone else??? - fey
-	-- whole lot of padding here
-	local contents = nodes.nodes[1].nodes[1].nodes
+-- 	--again, why are u hooking and not patching like everyone else??? - fey
+-- 	-- whole lot of padding here
+-- 	local contents = nodes.nodes[1].nodes[1].nodes
 
-	local scale = 0.4
-	local temp_col = G.C.DYN_UI.BOSS_MAIN
-	local temp_col2 = G.C.DYN_UI.BOSS_DARK
+-- 	local scale = 0.4
+-- 	local temp_col = G.C.DYN_UI.BOSS_MAIN
+-- 	local temp_col2 = G.C.DYN_UI.BOSS_DARK
 
-	-- add text for spark points
-	table.insert(contents,
-		{
-			n = G.UIT.R,
-			config = { align = "cm", id = 'row_spark' },
-			nodes = {
-                {
-                    n = G.UIT.C,
-                    config = { minw = 0.13 }
-                },
-				{
-					n = G.UIT.C,
-					config = { align = "cm", padding = 0.05, minw = 3, minh = 0.6, colour = temp_col, emboss = 0.05, r = 0.1 },
-					nodes = {
-						{
-							n = G.UIT.R,
-							config = { align = "cm" },
-							nodes = {
-								{
-									n = G.UIT.C,
-									config = { align = "cm", r = 0.1, minw = 2.85, minh = 0.5, colour = temp_col2, padding = -0.8 * scale, id = 'spark_text_UI', button = "hp_open_full_jx_top_up", func = 'hp_can_open_full_jx_top_up', hover = true },
-									nodes = {
-										{
-											n = G.UIT.C,
-											config = { align = "tl", minw = 2.85 },
-											nodes = {
-												{
-													n = G.UIT.R,
-													config = { minw = 0, minh = 0 }
-												},
-												{
-													n = G.UIT.T,
-													-- Having to add manual spaces fucking SUCKS
-													config = { text = '    ' .. localize('hotpot_spark_points'), colour = G.C.UI.TEXT_LIGHT, scale = 0.8 * scale, id = 'spark_text_label', shadow = true }
-												}
-											}
-										},
-										{
-											n = G.UIT.C,
-											config = { align = "cr", minw = 2.85 },
-											nodes = {
-												{
-													n = G.UIT.R,
-													config = { minw = 0.5, minh = 0 }
-												},
-												{
-													n = G.UIT.O,
-													config = {
-														object = DynaText {
-															-- i want the number to display in full until 1e10 or something so here
-															string = {
-																{ ref_table = G.GAME, ref_value = 'spark_points_display' }
-															},
-															colours = { G.C.BLUE },
-															shadow = true,
-															spacing = 2,
-															bump = true,
-															scale = 0.8 * scale
-														}
-													}
-												}
-											}
-										},
-									}
-								},
-							}
-						},
-					}
-				},
-			}
-		})
+-- 	-- add text for spark points
+-- 	table.insert(contents,
+-- 		{
+-- 			n = G.UIT.R,
+-- 			config = { align = "cm", id = 'row_spark' },
+-- 			nodes = {
+--                 {
+--                     n = G.UIT.C,
+--                     config = { minw = 0.13 }
+--                 },
+-- 				{
+-- 					n = G.UIT.C,
+-- 					config = { align = "cm", padding = 0.05, minw = 3, minh = 0.6, colour = temp_col, emboss = 0.05, r = 0.1 },
+-- 					nodes = {
+-- 						{
+-- 							n = G.UIT.R,
+-- 							config = { align = "cm" },
+-- 							nodes = {
+-- 								{
+-- 									n = G.UIT.C,
+-- 									config = { align = "cm", r = 0.1, minw = 2.85, minh = 0.5, colour = temp_col2, padding = -0.8 * scale, id = 'spark_text_UI', button = "hp_open_full_jx_top_up", func = 'hp_can_open_full_jx_top_up', hover = true },
+-- 									nodes = {
+-- 										{
+-- 											n = G.UIT.C,
+-- 											config = { align = "tl", minw = 2.85 },
+-- 											nodes = {
+-- 												{
+-- 													n = G.UIT.R,
+-- 													config = { minw = 0, minh = 0 }
+-- 												},
+-- 												{
+-- 													n = G.UIT.T,
+-- 													-- Having to add manual spaces fucking SUCKS
+-- 													config = { text = '    ' .. localize('hotpot_spark_points'), colour = G.C.UI.TEXT_LIGHT, scale = 0.8 * scale, id = 'spark_text_label', shadow = true }
+-- 												}
+-- 											}
+-- 										},
+-- 										{
+-- 											n = G.UIT.C,
+-- 											config = { align = "cr", minw = 2.85 },
+-- 											nodes = {
+-- 												{
+-- 													n = G.UIT.R,
+-- 													config = { minw = 0.5, minh = 0 }
+-- 												},
+-- 												{
+-- 													n = G.UIT.O,
+-- 													config = {
+-- 														object = DynaText {
+-- 															-- i want the number to display in full until 1e10 or something so here
+-- 															string = {
+-- 																{ ref_table = G.GAME, ref_value = 'spark_points_display' }
+-- 															},
+-- 															colours = { G.C.BLUE },
+-- 															shadow = true,
+-- 															spacing = 2,
+-- 															bump = true,
+-- 															scale = 0.8 * scale
+-- 														}
+-- 													}
+-- 												}
+-- 											}
+-- 										},
+-- 									}
+-- 								},
+-- 							}
+-- 						},
+-- 					}
+-- 				},
+-- 			}
+-- 		})
 
-	return nodes
-end
+-- 	return nodes
+-- end
