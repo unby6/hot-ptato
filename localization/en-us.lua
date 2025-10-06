e7ae2eac6c778ebf05054910c8fc3eb185a49151
Clicky- <84652396+Clickseee@users.noreply.github.com>
2025-10-07 01:05:57 +0700

    我正在偷偷享受你妈妈的服务
    Added Nxkoo's Credit

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -5310,7 +5310,23 @@ return {
                     },
                     {
                         name = "Nxkoo",
-                        text = {},
+                        text = {
+                            {
+                                "{C:green}Coder{} and {C:red}Ideas{}"
+                            },
+                            {
+                                "Responsible for {s:1.5,C:attention}67%{} of Horsechicot's {C:attention}Joker",
+                                "while also maintaining to be",
+                                "one of the most {s:1.5,C:red}ABYSMAL{} and {s:1.5,C:red}ATROCIOUS",
+                                "{C:attention}Developer{} ever participated. :3"
+                            },
+                            {
+                                "My Message:",
+                                "{C:inactive}I'm sorry, but as an AI language model,",
+                                "{C:inactive}I don't have consciousness or self-awareness.",
+                                "{C:inactive}I'm simply a program designed to process and generate human-like text."
+                            }
+                        },
                         atlas = "hpot_hc_apocalypse",
                         pos = { x = 3, y = 0 },
                     },
