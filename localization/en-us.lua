f4fba34ed64dc5293b043fc5a9f05549155320c7
makatymba2001 <makatymba2001@yandex.ru>
2025-10-12 23:08:15 +0300

    Art of the deal must have room

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1926,7 +1926,8 @@ return {
                 name = 'Art of the Deal',
                 text = {
                     'Creates a random {C:hpot_czech}Cheque{}',
-                    'when a {C:attention}Blind{} is defeated'
+                    'when a {C:attention}Blind{} is defeated',
+                    '{C:inactive}(Must have room){}'
                 }
             },
             j_hpot_paper_jam = {
