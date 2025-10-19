3d35579671bf9d5859cbf4f79946ea2c48e5f97b
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-10-19 13:49:02 +0100

    fix combat encounter for real

--- Pissdrawer/event_overhaul.lua
+++ Pissdrawer/event_overhaul.lua
@@ -297,8 +297,8 @@ function Blind:defeat(silent)
     if G.GAME.hpot_combat_event then
         if type(HotPotato.CombatEvents[G.GAME.hpot_combat_event].defeat) == "function" then
             HotPotato.CombatEvents[G.GAME.hpot_combat_event]:defeat()
-            if MP and MP.GAME then
-                MP.GAME.prevent_eval = false
+            if MP and MP.GAME then       
+                MP.GAME.round_ended = true
             end
         end
     end
