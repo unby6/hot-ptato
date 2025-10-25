fe18e1397bee15c627a791da8bc28c6dc064a0fc
makatymba2001 <makatymba2001@yandex.ru>
2025-10-25 15:21:34 +0300

    Prevent destruction triggers during tabs swap

--- Team Name/tname_hooks.lua
+++ Team Name/tname_hooks.lua
@@ -8,7 +8,7 @@ end
 
 local remove_old = Card.remove
 function Card:remove()
-    if self.added_to_deck or (self.area and (self.area == G.hand or self.area == G.deck)) then -- guys idfk what im doing please help me im begging please aaaaaaaaaaaaaaaaaaaaaa
+    if self.added_to_deck or (self.area and (self.area == G.hand or self.area == G.deck)) and not PissDrawer.Shop.is_tab_swap then -- guys idfk what im doing please help me im begging please aaaaaaaaaaaaaaaaaaaaaa
         SMODS.calculate_context({
             hpot_destroy = true,
             hpot_destroyed = self
