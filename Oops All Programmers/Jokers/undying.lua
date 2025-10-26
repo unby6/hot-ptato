47625cef65773fea93655a5c020e36eab8d12ab8
the-Astra <djs8743@rit.edu>
2025-10-25 21:43:10 -0400

    Put Undying respawn in an event (Fixes Hex crash)

--- Oops All Programmers/Jokers/undying.lua
+++ Oops All Programmers/Jokers/undying.lua
@@ -17,22 +17,27 @@ SMODS.Joker {
         }
     },
     loc_vars = function(self, info_queue, card)
-        return { vars = {  card.ability.extra.Xmult, card.ability.extra.gain } }
+        return { vars = { card.ability.extra.Xmult, card.ability.extra.gain } }
     end,
     calculate = function(self, card, context)
         if context.joker_type_destroyed and context.card == card and not context.blueprint then
-            local new_card = SMODS.add_card {
-                key = 'j_hpot_undying',
-            }
-            copy_card(card, new_card)
-            SMODS.scale_card (new_card, {
-                ref_table = new_card.ability.extra,
-                ref_value = 'Xmult',
-                scalar_table = card.ability.extra,
-                scalar_value = 'gain',
-                message_colour = G.C.MULT,
-                message_key = 'k_oap_undying'
-            })
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    local new_card = SMODS.add_card {
+                        key = 'j_hpot_undying',
+                    }
+                    copy_card(card, new_card)
+                    SMODS.scale_card(new_card, {
+                        ref_table = new_card.ability.extra,
+                        ref_value = 'Xmult',
+                        scalar_table = card.ability.extra,
+                        scalar_value = 'gain',
+                        message_colour = G.C.MULT,
+                        message_key = 'k_oap_undying'
+                    })
+                    return true;
+                end
+            }))
         end
 
         if context.joker_main and to_big(card.ability.extra.Xmult) > to_big(1) then
